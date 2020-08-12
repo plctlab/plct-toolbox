e46957bb1d53fb93fe5772f8488a7d9114a43899
Wei Wu <wuwei2016@iscas.ac.cn>
2020-08-12 09:42:36 +0800

    circt: build cmake itself

--- circt_init.sh
+++ circt_init.sh
@@ -6,6 +6,27 @@ set -e
 
 TOP="$PWD"
 
+ensure_cmake () {
+  echo "Make sure your cmake version is above 3.13.4:"
+  echo "your version is: `cmake --version`"
+  echo
+  echo "I will download and use the latest sable cmake,"
+  echo "Press any key other than enter to skip this step."
+  echo "or press ENTER to continue to download & build cmkae."
+  read -t 5 -p "You have 5 seconds to refuse, or I'll continue:"
+  [ x"$REPLY" = x"" ] || return
+  wget https://github.com/Kitware/CMake/releases/download/v3.18.1/cmake-3.18.1.tar.gz
+  cd cmake-3.18.1
+  [ -d "$HOME/bin" ] || mkdir -p "$HOME/bin"
+  ./configure --prefix="$HOME/bin" --parallel=$(nproc)
+  make install -j $(nproc)
+  export PATH="$HOME/bin/bin:$PATH"
+  echo 'export PATH="$HOME/bin/bin:$PATH"' >> $HOME/.bashrc
+
+}
+
+ensure_cmake
+
 git clone https://github.com/circt/circt.git
 cd circt
 sed -i 's,git@github.com:,https://github.com/,' .gitmodules
@@ -13,6 +34,7 @@ git submodule init
 git submodule update
 mkdir -p llvm/build
 cd llvm/build
+
 cmake -G Ninja ../llvm \
   -DLLVM_ENABLE_PROJECTS="mlir" \
   -DLLVM_TARGETS_TO_BUILD="X86;RISCV"  \
