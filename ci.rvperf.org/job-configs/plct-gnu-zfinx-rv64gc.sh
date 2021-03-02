db8149a2d4b15e2178824abc5c1ec821ead4fed3
Wei Wu <wuwei2016@iscas.ac.cn>
2021-03-02 16:06:20 +0800

    CI/plct-gnu-zfinx-rv64gc.sh: bugfix
    jenkins shell failed to recognize pushd.

--- ci.rvperf.org/job-configs/plct-gnu-zfinx-rv64gc.sh
+++ ci.rvperf.org/job-configs/plct-gnu-zfinx-rv64gc.sh
@@ -1,24 +1,24 @@
 git submodule update --init
 
 
-pushd riscv-gcc
+cd riscv-gcc
 git remote | grep -q zfinx || git remote add zfinx https://github.com/pz9115/riscv-gcc.git
 git fetch zfinx
 git checkout -f zfinx/riscv-gcc-10.2.0-zfinx
-popd
+cd ..
 
-pushd riscv-binutils
+cd riscv-binutils
 git remote | grep -q zfinx || git remote add zfinx https://github.com/pz9115/riscv-binutils-gdb.git
 git fetch zfinx
 git checkout zfinx/riscv-binutils-2.35-zfinx
-popd
+cd ..
 
-pushd qemu
+cd qemu
 git remote | grep -q plct-qemu || git remote add plct-qemu https://github.com/isrc-cas/plct-qemu.git || true
 git fetch plct-qemu
 git checkout plct-qemu/plct-zfinx-dev
 git reset --hard d73c46e4a84e47ffc61b8bf7c378b1383e7316b5
-popd
+cd ..
 
 # for rv64:
 ./configure --prefix="$PWD/opt-rv64/" --with-arch=rv64gc --with-abi=lp64 --with-multilib-generator="rv64gc-lp64--"
