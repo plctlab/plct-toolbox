b3429b2c01ce6f24010e88f45398688bd97ca300
Wei Wu <wuwei2016@iscas.ac.cn>
2021-03-02 17:29:22 +0800

    CI/riscv-gnu-rv64gc-master.sh: Update QEMU
    +# Temp Fix. See:
    +#    https://github.com/riscv/riscv-gnu-toolchain/issues/736

--- ci.rvperf.org/job-configs/riscv-gnu-rv64gc-master.sh
+++ ci.rvperf.org/job-configs/riscv-gnu-rv64gc-master.sh
@@ -1,6 +1,15 @@
 RV64_BIN="$PWD/obj-rv64"
 
 git submodule update --init
+
+# Temp Fix. See:
+#    https://github.com/riscv/riscv-gnu-toolchain/issues/736
+cd qemu
+git pull
+git submodule update --init --recursive
+cd ..
+# End of temp fix
+
 ./configure --prefix="$RV64_BIN"
 make linux -j $(nproc)
 
