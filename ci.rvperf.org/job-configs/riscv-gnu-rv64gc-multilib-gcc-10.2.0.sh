4030a54b66833987565f6826d8a0b703ef82a6ce
Wei Wu <wuwei2016@iscas.ac.cn>
2021-03-02 15:26:58 +0800

    Add new job
    riscv-gnu-rv64gc-multilib-gcc-10.2.0

---
+++ ci.rvperf.org/job-configs/riscv-gnu-rv64gc-multilib-gcc-10.2.0.sh
@@ -0,0 +1,13 @@
+git submodule update --init
+cd riscv-gcc
+git checkout riscv-gcc-10.2.0
+cd ..
+
+# test:
+./configure --prefix="$PWD/obj-rv64gc/" --enable-multilib
+
+# you can use make -j* to make speed up
+make -j $(nproc) check-gcc-newlib
+make -j $(nproc) check-binutils-newlib
+make report-gcc-newlib
+make report-binutils-newlib
