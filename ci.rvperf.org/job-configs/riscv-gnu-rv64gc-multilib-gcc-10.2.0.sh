175fd3bca14111a8ce0d32a968bef54853102f60
Wei Wu <wuwei2016@iscas.ac.cn>
2021-03-02 17:37:15 +0800

    CI/riscv-gnu-rv64gc-multilib-gcc-10.2.0.sh: fix qemu
    +# Temp Fix. See:
    +#    https://github.com/riscv/riscv-gnu-toolchain/issues/736

--- ci.rvperf.org/job-configs/riscv-gnu-rv64gc-multilib-gcc-10.2.0.sh
+++ ci.rvperf.org/job-configs/riscv-gnu-rv64gc-multilib-gcc-10.2.0.sh
@@ -3,7 +3,14 @@ cd riscv-gcc
 git checkout riscv-gcc-10.2.0
 cd ..
 
-git submodule update
+# Temp Fix. See:
+#    https://github.com/riscv/riscv-gnu-toolchain/issues/736
+cd qemu
+git checkout master
+git pull
+cd ..
+
+git submodule update --init --recursive
 
 # test:
 ./configure --prefix="$PWD/obj-rv64gc/" --enable-multilib
