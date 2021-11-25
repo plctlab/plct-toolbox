c905a5ddad5aa00836b7857c44cd94df337e5bbd
Wei Wu <wuwei2016@iscas.ac.cn>
2021-11-25 18:28:11 +0800

    Import two scripts used for OpenJDK team
    https://github.com/openjdk-riscv/jdk11u/wiki/External-Libraries

---
+++ openjdk/build_ext_libs_32.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# exit on error
+set -e
+
+if [ ! -n "$1" ];then
+    echo "Please designate riscv toolchain path"
+    exit 1
+else
+    riscvpath=$1
+    echo "riscv toolchian path was set as: $riscvpath"
+fi
+
+export PATH=$riscvpath/bin:$PATH
+export sysroot=$riscvpath/sysroot
+export prefix=$sysroot/usr
+
+# libffi
+cd libffi && ./autogen.sh && ./configure --host=riscv32-unknown-linux-gnu --prefix=$prefix 
+
+make && make install
+
+cd -
+
+# cups
+cd cups && ./configure --host=riscv32-unknown-linux-gnu --disable-ssl --disable-gssapi --disable-avahi --disable-libusb --disable-dbus --disable-systemd
+
+make CFLAGS="-Wno-error=sign-conversion -Wno-error=format-truncation" CXXFLAGS="-Wno-error=sign-conversion -Wno-error=format-truncation" && make install DSTROOT=$sysroot
+
+cd -
+
+# libexpat
+cd libexpat/expat && ./buildconf.sh &&./configure --host=riscv32-unknown-linux-gnu --prefix=$prefix
+
+make && make install
+
+cd -
+
+# zlib
+cd zlib && CHOST=riscv32 CC=riscv32-unknown-linux-gnu-gcc AR=riscv32-unknown-linux-gnu-ar RANLIB=riscv32-unknown-linux-gnu-ranlib ./configure  --prefix=$prefix
+
+make && make install
+
+cd -
+
+# libpng
+cd libpng && ./configure --host=riscv32-unknown-linux-gnu --prefix=$prefix
+
+make && make install
+
+cd -
+
+# freetype2
+cd freetype2 && ./autogen.sh && ./configure --host=riscv32-unknown-linux-gnu --prefix=$prefix --with-brotli=no --with-harfbuzz=no --with-bzip2=no
+
+make && make install
+
+cd -
+
+# json-c
+cd json-c && ./autogen.sh &&  ./configure --host=riscv32-unknown-linux-gnu --prefix=$prefix
+
+make && make install
+
+cd -
+
+# fontconfig
+cd fontconfig && PKG_CONFIG_PATH=$prefix/lib/pkgconfig ./autogen.sh --host=riscv32-unknown-linux-gnu --prefix=$prefix
+
+make && make install
+
+cd -
+
+# alsa-lib
+cd alsa-lib && libtoolize --force --copy --automake && aclocal && autoheader && automake --foreign --copy --add-missing && autoconf && ./configure --host=riscv32-unknown-linux-gnu --prefix=$prefix
+
+make && make install
+
+cd -
+
+# util-linux
+cd util-linux && ./autogen.sh && ./configure --host=riscv32-unknown-linux-gnu --prefix=$prefix --disable-all-programs --enable-libuuid
+
+make && make install || true
+
+cd -
+
+# xorg
+cd xorg && CONFFLAGS="--host=riscv32-unknown-linux-gnu --disable-malloc0returnsnull" ./util/modular/build.sh --modfile ./xorg_modules --clone $prefix
+
+echo "Success. exit"
+
---
+++ openjdk/manual_install_deps.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+mkdir build_ext_libs_riscv32 && cd build_ext_libs_riscv32
+
+git clone --depth=1 https://github.com/libffi/libffi
+
+git clone --depth=1 https://github.com/apple/cups
+
+git clone --depth=1 https://github.com/libexpat/libexpat
+
+git clone --depth=1 https://github.com/madler/zlib
+
+git clone --depth=1 https://github.com/glennrp/libpng
+
+wget https://download.savannah.gnu.org/releases/freetype/freetype-2.10.4.tar.gz && tar -xzvf freetype-2.10.4.tar.gz && mv freetype-2.10.4 freetype2 && rm -f freetype-2.10.4.tar.gz
+
+git clone -b json-c-0.13 --depth=1 https://github.com/json-c/json-c
+
+git clone --depth=1 https://gitlab.freedesktop.org/fontconfig/fontconfig
+
+git clone --depth=1 https://github.com/alsa-project/alsa-lib
+
+git clone --depth=1 https://github.com/karelzak/util-linux
+
+mkdir xorg && cd xorg && wget https://raw.githubusercontent.com/openjdk-riscv/xorg-util-modular/riscv32/xorg_modules && git clone --depth=1 -b riscv32 https://github.com/openjdk-riscv/xorg-util-modular util/modular
+
+cd ..
+
+
